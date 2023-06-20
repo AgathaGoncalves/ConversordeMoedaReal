6490b93e3ee0da5f2c6486acf652d33beec3c32a
Matheus Boaventura Prieto <70549288+MatheusBoaventura@users.noreply.github.com>
2023-06-19 23:05:10 -0300

    Update conversoes.py
    Conversão dolar.

--- conversoes.py
+++ conversoes.py
@@ -2,3 +2,9 @@ def converterYen(moedaReal):
     cambioYen = 29.74
     moedaReal = moedaReal*cambioYen
     return moedaReal
+    
+def converterDolar(moedaReal):
+    cambioDolar = 0.21
+    moedaReal = moedaReal*cambioDolar
+    print('Teste da dolar', moedaReal)
+    return moedaReal
